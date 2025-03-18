635345de55f17d4030cb3710f04b6598b2154105
gumieProf <gutsmine910@gmail.com>
2025-03-18 12:40:36 +0900

    FIX SH(Change YT-DLP)

--- make_gif.sh
+++ make_gif.sh
@@ -12,7 +12,7 @@ dur=$4
 size=430
 vname="_a.mp4"
 
-youtube-dl -f "mp4" -o $vname $1
+yt-dlp -f "mp4" -o $vname $1
 ffmpeg -loglevel warning -ss $stt -t $dur -y -i $vname -vf "fps=$fps,scale=$size:-1:flags=lanczos,palettegen" palette.png
 ffmpeg -loglevel warning -ss $stt -t $dur -y -i $vname -i palette.png \
     -filter_complex "fps=$fps,scale=$size:-1:flags=lanczos[x];[x][1:v]paletteuse" $5
